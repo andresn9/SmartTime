ff6b979a07adb9256b96ee3b77dca6b8ba885473
Kraze <Kraze@PHANTEKS>
2020-11-05 01:13:49 +0100

    hecho el layout de amigos

---
+++ app/src/main/java/com/example/smarttime/Amigos.kt
@@ -0,0 +1,12 @@
+package com.example.smarttime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Amigos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.amigos)
+    }
+
+}
