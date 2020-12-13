6d487431bc898a0e3c7d939ff562c85948384388
Kraze <Kraze@PHANTEKS>
2020-12-13 17:12:11 +0100

    creacion y diseño prelimiar de amigos

---
+++ app/src/main/java/com/norbertovargas/pruebassmarttime/ActivityAmigos.kt
@@ -0,0 +1,11 @@
+package com.norbertovargas.pruebassmarttime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityAmigos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_amigos)
+    }
+}
