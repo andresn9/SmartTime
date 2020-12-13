77f65ad786589c5b1cb3332cb1b415ffb8def1e1
Kraze <Kraze@PHANTEKS>
2020-12-13 18:23:11 +0100

    pequeño cambio al diseño del recycler view y ajustes en su adaptador (inútil por ahora) para probar como se ve

--- app/src/main/java/com/norbertovargas/pruebassmarttime/ActivityAmigos.kt
+++ app/src/main/java/com/norbertovargas/pruebassmarttime/ActivityAmigos.kt
@@ -2,10 +2,24 @@ package com.norbertovargas.pruebassmarttime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ActivityAmigos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_amigos)
+
+        var contenedor: RecyclerView = this.findViewById(R.id.listaAmigos)
+        contenedor.removeAllViewsInLayout()
+        var cadena:String = "hola"
+
+        var recV: RecyclerView = findViewById(R.id.listaAmigos)
+        var miAdaptador:AmigosAdapter = AmigosAdapter(this, cadena)
+        var linearLayoutManager: LinearLayoutManager
+
+        recV.adapter = miAdaptador
+        recV.layoutManager = LinearLayoutManager(this)
     }
 }
